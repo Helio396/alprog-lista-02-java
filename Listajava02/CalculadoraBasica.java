27876332ce1013a4f9b9ccffa609db8cacb1d8aa
Helio396 <heliosantosmendonca@gmail.com>
2026-03-19 10:38:00 -0300

    Update CalculadoraBasica.java

--- Listajava02/CalculadoraBasica.java
+++ Listajava02/CalculadoraBasica.java
@@ -3,19 +3,19 @@ import java.util.Scanner;
 public class CalculadoraBasica {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // Declaração das variaveis que pode receber numero decimal graças ao double
+          // Declaração das variaveis que pode receber numero decimal graças ao double
         double numero1, numero2, soma, subtracao, multiplicacao;
-        // Imprimir as mensagens para o usuario gerar informação
+          // Imprimir as mensagens para o usuario gerar informação
         System.out.print("Digite o primeiro número: ");
         numero1 = scanner.nextDouble();
 
         System.out.print("Digite o segundo número: ");
         numero2 = scanner.nextDouble();
-        // Atribui o valor das minhas variaveis
+          // Atribui o valor das minhas variaveis
         soma = numero1 + numero2;
         subtracao = numero1 - numero2;
         multiplicacao = numero1 * numero2;
-        // Imprimir as mensagens que testei no método formatado
+          // Imprimir as mensagens que testei no método formatado
         System.out.println("\n" + "=".repeat(40));
         System.out.printf("CALCULOS COM %.2f E %.2f%n", numero1, numero2);
         System.out.println("=".repeat(40));
@@ -23,7 +23,7 @@ public class CalculadoraBasica {
         System.out.printf("Subtracao: %.2f - %.2f = %.2f%n", numero1, numero2, subtracao);
         System.out.printf("Multiplicacao: %.2f x %.2f = %.2f%n", numero1, numero2, multiplicacao);
         System.out.println("=".repeat(40));
-        // fechei minha biblioteca scanner
+          // fechei minha biblioteca scanner
         scanner.close();
     }
 }
