83ed5b5f42327c36a160251bf3522d7dfdbf3b43
Helio396 <heliosantosmendonca@gmail.com>
2026-03-19 10:41:26 -0300

    Update NumeroInformado.java

--- Listajava02/NumeroInformado.java
+++ Listajava02/NumeroInformado.java
@@ -2,15 +2,14 @@ import java.util.Scanner;
 
 public class NumeroInformado {
     public static void main(String[] args) {
-        // Criando o objeto scanner para ler os dado digitado pelo usuário
-        Scanner scanner = new Scanner(System.in);
+           // Criando o objeto scanner para ler os dado digitado pelo usuário
+      Scanner scanner = new Scanner(System.in);
         System.out.print("Digite um número? ");
-        // Declarei minha variável que pode receber número decimal graças a variável
-        // double
-        double numero = scanner.nextDouble();
-        // Imprimir mensagem mais o número informado pelo usuário
+          // Declarei minha variável que pode receber número decimal graças a variável double
+      double numero = scanner.nextDouble();
+          // Imprimir mensagem mais o número informado pelo usuário
         System.out.print("O número informado é: " + numero + ".");
-        // Por fim fechei minha biblioteca scanner
+          // Por fim fechei minha biblioteca scanner
         scanner.close();
     }
 }
