5e146fd7ccc85bb65737b7a88d0db0f3e6bd9d07
Helio396 <heliosantosmendonca@gmail.com>
2026-03-19 10:40:25 -0300

    Update MediaNotas.java

--- Listajava02/MediaNotas.java
+++ Listajava02/MediaNotas.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 
 public class MediaNotas {
     public static void main(String[] args) {
-        // Declaração do meu metodo main que seria o inicio do programa no portugol
+          // Declaração do meu metodo main que seria o inicio do programa no portugol
         Scanner scanner = new Scanner(System.in);
-        // Declarei minhas variaveis do tipo decimal pois pode ter nota com casa decimal
+          // Declarei minhas variaveis do tipo decimal pois pode ter nota com casa decimal
         double nota1, nota2, nota3, nota4, media;
         /*
          * imprimo a mensagem abaixo e atribuo o valor que o usuário responder para
@@ -12,16 +12,16 @@ public class MediaNotas {
          * da mesma forma do leia no portugol
          */
         System.out.print("Digite a primeira nota bimestral: ");
-        nota1 = scanner.nextDouble();
+           nota1 = scanner.nextDouble();
 
         System.out.print("Digite a segunda nota bimestral: ");
-        nota2 = scanner.nextDouble();
+           nota2 = scanner.nextDouble();
 
         System.out.print("Digite a terceira nota bimestral: ");
-        nota3 = scanner.nextDouble();
+           nota3 = scanner.nextDouble();
 
         System.out.print("Digite a quarta nota bimestral: ");
-        nota4 = scanner.nextDouble();
+           nota4 = scanner.nextDouble();
         /*
          * Atribui o valor para media e sempre usando parenteses para não realizar a
          * divisão primeiro
